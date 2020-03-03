c7adfe3570bef199b7923c6b541bb4d6a324aa4a
Archeelux <archeelux@gmail.com>
2020-03-03 17:42:02 +0000

    added counter and knockoutjs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,11 @@
-"use strict";
+const ko = require("knockout");
 
-const $ = el => {
-  document.querySelector(el);
+const vw = function() {
+  this.fuel = ko.observable(0);
+
+  this.clickFuel = function() {
+    this.fuel(this.fuel() + 1);
+  };
 };
+
+ko.applyBindings(new vw());

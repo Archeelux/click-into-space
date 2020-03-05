142d98d8d04a7aef13a184679c447d865b71aba9
Archeelux <archeelux@gmail.com>
2020-03-05 16:58:16 +0000

    added launch option

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,15 @@ const ko = require("knockout");
 
 const vw = function() {
   this.fuel = ko.observable(0);
+  this.speed = ko.observable(0);
 
   this.clickFuel = function() {
     this.fuel(this.fuel() + 1);
+  };
+
+  this.clickSpeed = function() {
+    this.speed(this.fuel() / 100);
+    this.fuel(0);
   };
 };
 

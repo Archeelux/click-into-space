22f0cfec6d6523d87b6643593ebc3128845769cd
Archeelux <archeelux@gmail.com>
2020-03-05 17:27:52 +0000

    added timer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,12 @@
 const ko = require("knockout");
 
 const vw = function() {
+  const self = this;
+
   this.fuel = ko.observable(0);
   this.speed = ko.observable(0);
+  this.money = ko.observable(0);
+  this.moneyPS = ko.observable(0);
 
   this.clickFuel = function() {
     this.fuel(this.fuel() + 1);
@@ -10,8 +14,16 @@ const vw = function() {
 
   this.clickSpeed = function() {
     this.speed(this.fuel() / 100);
+    this.moneyPS(this.speed() / 2);
     this.fuel(0);
   };
+
+  this.earnMoney = function() {
+    console.log(self.money());
+    self.money(self.money() + self.moneyPS());
+  };
+
+  setInterval(self.earnMoney, 1000);
 };
 
 ko.applyBindings(new vw());

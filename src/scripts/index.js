ea5cb71eac67b532f5ba84a34fbf5fc319590d9c
archeelux <archeelux@gmail.com>
2020-03-25 12:42:37 +0000

    auto fuel upgrade initialized

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,23 +7,36 @@ const vw = function() {
   this.speed = ko.observable(0);
   this.money = ko.observable(0);
   this.moneyPS = ko.observable(0);
+  this.autoFuelCost = ko.observable(10);
+  this.autoFuelCount = ko.observable(0);
+  this.autoFuelPS = ko.observable(0);
 
   this.clickFuel = function() {
     this.fuel(this.fuel() + 1);
   };
 
   this.clickSpeed = function() {
-    this.speed(this.fuel() / 100);
+    this.speed(this.speed() + this.fuel() / 100);
     this.moneyPS(this.speed() / 2);
     this.fuel(0);
   };
 
   this.earnMoney = function() {
-    console.log(self.money());
     self.money(self.money() + self.moneyPS());
   };
 
+  this.autoFuel = function() {
+    self.fuel(self.fuel() + self.autoFuelPS());
+  };
+
+  this.clickBuyAutoFuelUpgrade = function() {
+    this.autoFuelCost(this.autoFuelCost() + 1);
+    this.autoFuelCount(this.autoFuelCount() + 1);
+    this.autoFuelPS(this.autoFuelCount());
+  };
+
   setInterval(self.earnMoney, 1000);
+  setInterval(self.autoFuel, 1000);
 };
 
 ko.applyBindings(new vw());
